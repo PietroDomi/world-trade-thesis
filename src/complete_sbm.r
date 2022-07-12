bc8bf7dbbd6f48cff255ffca4cdcc2e61e97407e
PietroDomi <pietro.dominietto@gmail.com>
2022-07-12 10:36:17 +0200

    v2 | ignore ini

--- src/complete_sbm.r
+++ src/complete_sbm.r
@@ -1,5 +1,5 @@
 getwd()
-setwd("/content/gdrive/MyDrive/COMEXT")
+setwd("H:/My Drive/COMEXT")
 getwd()
 library(future, quietly = TRUE) # allows parralel processing in greed()
 library(Matrix, quietly = TRUE) # sparse matrix
@@ -15,10 +15,10 @@ thresh <- read.csv("data-samples/manual/complete_thresholds.csv", row.names = "X
 files <- list.files("data-samples/graphs/complete")
 # pb <- txtProgressBar(min = 0, max = length(files), initial = 0)
 for (i  in 1:length(files)){
-    chars = "2021"
+    chars = ""
     f <- files[i]
     print(paste(i,"/",length(files)))
-    if (grepl(chars, f, fixed = TRUE)){
+    if (grepl(chars, f, fixed = TRUE)&!(grepl(chars, "ini", fixed = TRUE))){
         name <- paste("data-samples/graphs/complete/", f, sep = "")
         x <- unlist(strsplit(f, "_"))
         y <- substr(x[2], 2, 5)
@@ -26,7 +26,7 @@ for (i  in 1:length(files)){
         g <- read.gexf(name)
         g_ig <- gexf.to.igraph(g)
         bin_mat <- as(as.matrix((as_adj(g_ig,attr="weight") >= thresh[p,paste("X",y,sep="")])),"dgCMatrix")
-        sol <- greed(bin_mat,model=Sbm(alpha=1,a0=1,b0=1,type="directed"),verbose=FALSE)
+        sol <- greed(bin_mat, model = Sbm(alpha = 1, a0 = 5, b0 = 2, type = "directed"), K = 2, verbose = FALSE)
         v <- V(g_ig)$name
         c <- clustering(sol)
         s <- c("country,block")
@@ -34,7 +34,7 @@ for (i  in 1:length(files)){
             s <- c(s,paste(v[i],c[i],sep=","))
         }
         f_o <- unlist(strsplit(f, "[.]"))
-        out_name <- paste("data-samples/sbm/", f_o[1], ".csv", sep = "")
+        out_name <- paste("data-samples/sbm/v2/", f_o[1], ".csv", sep = "")
         fileConn <- file(out_name)
         writeLines(s, fileConn)
         close(fileConn)
